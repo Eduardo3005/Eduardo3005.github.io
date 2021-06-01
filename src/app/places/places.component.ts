632d267d7acaa3863f592fbfae46502834110337
eduardo.borrego <eduardo.borrego@parceiros.nos.pt>
2021-06-01 14:02:22 +0100

    Trying to fix places page

--- src/app/places/places.component.ts
+++ src/app/places/places.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-
+import { select, Store } from '@ngrx/store';
+import { TranslateService } from '@ngx-translate/core';
+import { take, tap } from 'rxjs/operators';
+import { AppState } from '../state/app-state';
+import { selectLanguage } from '../state/language/language-state';
 @Component({
   selector: 'app-places',
   templateUrl: './places.component.html',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PlacesComponent implements OnInit {
 
-  constructor() {}
+  constructor(
+    private readonly store: Store<AppState>,
+    private readonly translate: TranslateService) {}
 
   ngOnInit(): void {
+
+    this.store
+    .pipe(
+      select(selectLanguage),
+      tap((language: string) => {
+        this.translate.use(language)
+      }),
+      take(1)
+    ).subscribe()
   }
 
 }

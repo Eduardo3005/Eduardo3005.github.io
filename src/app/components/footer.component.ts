df802f42243fbc413a344485e552fd6aa6eae21e
eduardo.borrego <eduardo.borrego@parceiros.nos.pt>
2021-06-10 11:34:12 +0100

    fixing cofinacing modal ad adding maps link

--- src/app/components/footer.component.ts
+++ src/app/components/footer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
-  constructor() {}
+  googleMapsIconPath : string;
+
+  constructor(
+    readonly translate: TranslateService,
+  ) {
+  }
 
   ngOnInit(): void {
+
+    this.translate.get('Footer.GoogleMapsIconPath').subscribe((res: string) => {
+      this.googleMapsIconPath = res;
+    });
   }
 
 }

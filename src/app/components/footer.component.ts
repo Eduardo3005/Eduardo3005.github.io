2b40d54bfcf8b0187e0d709089bc53f3949f5e68
eduardo.borrego <eduardo.borrego@parceiros.nos.pt>
2021-06-10 13:48:29 +0100

    Updating footer text origin

--- src/app/components/footer.component.ts
+++ src/app/components/footer.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { TranslateService } from '@ngx-translate/core';
+import { FirebaseProvider } from '../services/firebase-logger.service';
+import { DynamicComponent } from '../shared/base.component';
+import { AppState } from '../state/app-state';
 
 @Component({
   selector: 'app-footer',
   templateUrl: '../../assets/templates/footer/index.html',
-  styleUrls: ['../../assets/templates/footer/style.scss']
+  styleUrls: ['../../assets/templates/footer/style.scss'],
 })
-export class FooterComponent implements OnInit {
-
-  googleMapsIconPath : string;
+export class FooterComponent extends DynamicComponent implements OnInit {
+  googleMapsIconPath: string;
 
   constructor(
+    readonly store: Store<AppState>,
     readonly translate: TranslateService,
+    readonly firebase: FirebaseProvider
   ) {
+    super(store, translate, firebase);
   }
 
   ngOnInit(): void {
-
     this.translate.get('Footer.GoogleMapsIconPath').subscribe((res: string) => {
       this.googleMapsIconPath = res;
     });
   }
-
 }

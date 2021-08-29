421084104a73a75dfacab8d20adb45f8def04f53
eduardo.borrego <eduardo.borrego@parceiros.nos.pt>
2021-08-29 18:13:46 +0100

    Adding contacts page and removing other stuff

--- docs/assets/base.js
+++ docs/assets/base.js
@@ -1,7 +1,21 @@
 $(document).ready(function () {
   $(document).click(function () {
-     // if($(".navbar-collapse").hasClass("in")){
-       $('.navbar-collapse').collapse('hide');
-     // }
+    // if($(".navbar-collapse").hasClass("in")){
+    $(".navbar-collapse").collapse("hide");
+    // }
+  });
+
+  $("#playvideo").click(function () {
+    $(".youtube-video-play").hide();
+
+    $(".youtube-video").show();
+
+    setTimeout(function () {
+
+    }, 2000);
+
+
+    $("#video")[0].src += "?autoplay=1";
   });
 });
+

f03d79b4e3a29b068aac2dba6a009c82c0e59ffb
eduardo3005 <eduardo3005@gmail.com>
2022-05-16 00:32:28 +0100

    Adding descriptions and new photos

--- src/app/models/reference.ts
+++ src/app/models/reference.ts
@@ -3,11 +3,11 @@ import { Image } from "./image";
 export class Reference {
     Name: string;
     Description: string;
-    Images: Array<Image>;
+    MainImage: Image;
     Recomendations: Array<Recomendation>
     Link: string;
   }
-  
+
   export class Recomendation {
     IconPath: string;
     Text: string;
